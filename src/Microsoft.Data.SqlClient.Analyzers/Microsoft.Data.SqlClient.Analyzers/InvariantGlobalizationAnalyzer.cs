c96f6c12547dfaa61152c7d9d6587d8cc821adaf
H. Yeo <138053434+H-Yeo@users.noreply.github.com>
2023-12-29 11:27:52 -0800

    Clean-up Pre-Testing
    Still not sure whether this would work, but cleaning up code to make this easier to read.

--- src/Microsoft.Data.SqlClient.Analyzers/Microsoft.Data.SqlClient.Analyzers/InvariantGlobalizationAnalyzer.cs
+++ src/Microsoft.Data.SqlClient.Analyzers/Microsoft.Data.SqlClient.Analyzers/InvariantGlobalizationAnalyzer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
@@ -12,8 +11,6 @@ namespace Microsoft.Data.SqlClient.Analyzers
     {
         public const string DiagnosticId = "InvariantGlobalizationAnalyzer";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
@@ -27,26 +24,7 @@ namespace Microsoft.Data.SqlClient.Analyzers
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
-
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            // context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
             context.RegisterCompilationStartAction(CompilationStart);
-        }
-
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-            {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-                context.ReportDiagnostic(diagnostic);
-            }
         }
 
         private static void CompilationStart(CompilationStartAnalysisContext context)
@@ -56,7 +34,7 @@ namespace Microsoft.Data.SqlClient.Analyzers
                 throw new ArgumentNullException(nameof(context));
             }
 
-            bool isInvariantGlobalization = "Á".Equals("á", StringComparison.CurrentCultureIgnoreCase)
+            bool isInvariantGlobalization = "Á".Equals("á", StringComparison.CurrentCultureIgnoreCase);
 #if NET6_0_OR_GREATER
             // Microsoft Documentation on Breaking Changes for Invariant Globalization
             // https://learn.microsoft.com/en-us/dotnet/core/compatibility/globalization/6.0/culture-creation-invariant-mode
@@ -82,7 +60,6 @@ namespace Microsoft.Data.SqlClient.Analyzers
         private static bool isNativeAheadOfTimeCompilation()
         {
             bool isNativeAOT = false;
-
 #if NET6_0_OR_GREATER  
         var stackTrace = new StackTrace(false);
         isNativeAOT = stackTrace.GetFrame(0)?.GetMethod() is null;
