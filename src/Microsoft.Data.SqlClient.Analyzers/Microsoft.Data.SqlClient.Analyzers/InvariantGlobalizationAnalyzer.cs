079da42c360f898eee9c8ed4f4c017c39a4d34eb
H. Yeo <138053434+H-Yeo@users.noreply.github.com>
2023-12-29 11:08:00 -0800

    Stubbing out the detection of InvariantGlobalization
    Per MS documentation, this is the test they use to determine Invariant Globalization based on .NET Version

--- src/Microsoft.Data.SqlClient.Analyzers/Microsoft.Data.SqlClient.Analyzers/InvariantGlobalizationAnalyzer.cs
+++ src/Microsoft.Data.SqlClient.Analyzers/Microsoft.Data.SqlClient.Analyzers/InvariantGlobalizationAnalyzer.cs
@@ -1,26 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace Microsoft.Data.SqlClient.Analyzers
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class InvariantGlobalizationAnalyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "MicrosoftDataSqlClientAnalyzers";
+        public const string DiagnosticId = "InvariantGlobalizationAnalyzer";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private const string Category = "Microsoft.Data.SqlClient.Analyzers";
 
         private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
@@ -33,7 +30,8 @@ namespace Microsoft.Data.SqlClient.Analyzers
 
             // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
             // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            // context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterCompilationStartAction(CompilationStart);
         }
 
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
@@ -49,6 +47,36 @@ namespace Microsoft.Data.SqlClient.Analyzers
 
                 context.ReportDiagnostic(diagnostic);
             }
+        }
+
+        private static void CompilationStart(CompilationStartAnalysisContext context)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            bool isInvariantGlobalization = "Á".Equals("á", StringComparison.CurrentCultureIgnoreCase);
+            bool isNativeAOT = false; //TODO: We need to figure out how this isNativeAOT?
+
+#if NET6_0_OR_GREATER
+            // Microsoft Documentation on Breaking Changes for Invariant Globalization
+            // https://learn.microsoft.com/en-us/dotnet/core/compatibility/globalization/6.0/culture-creation-invariant-mode
+#else
+            isInvariantGlobalization = true;
+#endif
+
+            context.RegisterOperationAction(context =>
+            {
+                var invocation = (IInvocationOperation)context.Operation;
+
+                if (isNativeAOT && isInvariantGlobalization)
+                {
+                    var diagnostic = Diagnostic.Create(Rule, invocation.Syntax.GetLocation());
+                    context.ReportDiagnostic(diagnostic);
+                }
+
+            }, OperationKind.Invocation);
         }
     }
 }
