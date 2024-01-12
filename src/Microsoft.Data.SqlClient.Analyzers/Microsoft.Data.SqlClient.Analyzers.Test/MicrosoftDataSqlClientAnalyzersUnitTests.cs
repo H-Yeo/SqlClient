ac66cbc32dcf4e86d4a3bb7cd6be418f55394dac
H. Yeo <138053434+H-Yeo@users.noreply.github.com>
2024-01-11 17:20:23 -0800

    Adding Roslyn Analyzer via new Project.
    Manual Adjustments needed to add to these resources.

---
+++ src/Microsoft.Data.SqlClient.Analyzers/Microsoft.Data.SqlClient.Analyzers.Test/MicrosoftDataSqlClientAnalyzersUnitTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Data.SqlClient.Analyzers.Test
+{
+    [TestClass]
+    public class MicrosoftDataSqlClientAnalyzersUnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            //var test = @"";
+
+            //await VerifyCS.VerifyAnalyzerAsync(test);
+            await Task.CompletedTask;
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            await Task.CompletedTask;
+    //        var test = @"
+    //using System;
+    //using System.Collections.Generic;
+    //using System.Linq;
+    //using System.Text;
+    //using System.Threading.Tasks;
+    //using System.Diagnostics;
+
+    //namespace ConsoleApplication1
+    //{
+    //    class {|#0:TypeName|}
+    //    {   
+    //    }
+    //}";
+
+    //        var fixtest = @"
+    //using System;
+    //using System.Collections.Generic;
+    //using System.Linq;
+    //using System.Text;
+    //using System.Threading.Tasks;
+    //using System.Diagnostics;
+
+    //namespace ConsoleApplication1
+    //{
+    //    class TYPENAME
+    //    {   
+    //    }
+    //}";
+
+            //var expected = VerifyCS.Diagnostic("MicrosoftDataSqlClientAnalyzers").WithLocation(0).WithArguments("TypeName");
+            //await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
