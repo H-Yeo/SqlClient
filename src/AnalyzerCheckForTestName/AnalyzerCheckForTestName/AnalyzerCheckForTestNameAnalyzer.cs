fd5c188e9511d8fa51274e8cb745732a98cf61fd
H. Yeo <138053434+H-Yeo@users.noreply.github.com>
2024-01-19 16:40:22 -0800

    This is a slightly Different Approach to processing with an Analyzer
    Checking in this code - But this is a breaking change. The analyzers are running in a Foreach Loop, the end-result is it creates some crazy compilation, which pretty much kills your machine. The focus here, is the start/stop Compilation for the Analyzer, which is why this code is added. It's analysis only, and should be changed.

--- src/AnalyzerCheckForTestName/AnalyzerCheckForTestName/AnalyzerCheckForTestNameAnalyzer.cs
+++ src/AnalyzerCheckForTestName/AnalyzerCheckForTestName/AnalyzerCheckForTestNameAnalyzer.cs
@@ -15,14 +15,12 @@ namespace AnalyzerCheckForTestName
     {
         public const string DiagnosticId = "AnalyzerCheckForTestName";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Naming";
 
-        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        public static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
@@ -31,20 +29,28 @@ namespace AnalyzerCheckForTestName
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
 
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterCompilationStartAction(startContext =>
+            {
+                startContext.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+
+                startContext.RegisterCompilationEndAction(endContext =>
+                {
+                    var compilationWithAnalyzer = endContext.Compilation.WithAnalyzers(ImmutableArray.Create<DiagnosticAnalyzer>(this));
+                    var diagnostics = compilationWithAnalyzer.GetAnalyzerDiagnosticsAsync().Result;
+                    foreach (var diagnostic in diagnostics)
+                    {
+                        endContext.ReportDiagnostic(diagnostic);
+                    }
+                });
+            });
         }
 
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
             var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
 
-            // Find just those named type symbols with names containing lowercase letters.
             if (namedTypeSymbol.Name == "TestName")
             {
-                // For all such symbols, produce a diagnostic.
                 var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
 
                 context.ReportDiagnostic(diagnostic);
---
+++ src/MyAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AnalyzerCheckForTestName
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MyAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(AnalyzerCheckForTestNameAnalyzer.Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+
+            if (namedTypeSymbol.Name == "TestName")
+            {
+                var diagnostic = Diagnostic.Create(AnalyzerCheckForTestNameAnalyzer.Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+
+
+}
